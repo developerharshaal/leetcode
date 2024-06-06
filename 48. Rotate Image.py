c94add11546306c204a353cb92699b816c6b6ad8
Harshaal Bajaj <118446013+developerharshaal@users.noreply.github.com>
2024-06-06 16:40:15 +0530

    Add files via upload

---
+++ 48. Rotate Image.py
@@ -0,0 +1,20 @@
+# BRUTE FORCE: USE DUMMY MATRIX
+# TIME COMPLEXITY: O(N^2)
+# SPACE COMPLEXITY: O(1)
+
+# OPTIMAL
+# TIME COMPLEXITY: O(N^2)
+# SPACE COMPLEXITY: O(1)
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+
+        for i in range(n):
+            matrix[i].reverse()
+            
